dfdbc101a3211fd36dfcb346dc51f2f2bd4adaf7
madsandm <madsandm@stud.ntnu.no>
2024-02-22 11:54:39 +0100

    heis init safeguard

--- source/driver/elevator.c
+++ source/driver/elevator.c
@@ -1,6 +1,10 @@
 #include "elevator.h"
 
 void elevatorInit(Elevator *heis) {
+    while (elevatorGetPosition(heis) == -1)
+    {
+        elevio_motorDirection(DIRN_DOWN);
+    }
     heis->currentFloor = elevatorGetPosition(heis);
     heis->door_Open = 0;
 }
