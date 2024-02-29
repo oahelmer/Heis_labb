920570d5e723a6b2680cf54c7cb2b9cbfb54b270
oahelmer <oahelmer@stud.ntnu.no>
2024-02-29 10:26:29 +0100

    Heis funker, lys mangler men etasjelyslister lagt til

--- source/main.c
+++ source/main.c
@@ -10,27 +10,31 @@
 
 int etasjeliste_opp[] = {0,0,0,0};
 int etasjeliste_ned[] = {0,0,0,0};
+int lysliste_opp[] = {0,0,0,0};
+int lysliste_ned[] = {0,0,0,0};
+int lysliste_inne[] = {0,0,0,0};
+bool lysFlagg = 0;
 
 int main(){
     elevio_init();
     MotorDirection direction = DIRN_STOP; // bevegelses retning
     Elevator heis;
     heis.direction = DIRN_UP; // prioritert retning
+    int forrigeEtasje = 0;
 
     printf("=== Example Program ===\n");
     printf("Press the stop button on the elevator panel to exit\n");
 
 //    elevio_motorDirection(direction);
-    elevatorInit(&heis);
-
+/*     elevatorInit(&heis);
+ */
     //Startup the elevator goes down until floorstate is defined
     while(elevio_floorSensor() == -1){
-        elevio_motorDirection(direction);
-    }
+        nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
+        elevio_motorDirection(DIRN_DOWN);
+    }elevio_motorDirection(DIRN_STOP);
 
     while(1){
-        int floorState = elevio_floorSensor();
-
         /*
         if(heis.currentFloor == 0){
             elevio_motorDirection(DIRN_UP);
@@ -51,13 +55,15 @@ int main(){
             }
         }
 
+        int floorState = elevio_floorSensor();
 
         // Sjekker om heisen ikke er i etasje setter currentFloor og etasjelys.
-        if(elevio_floorSensor() != -1){
-            heis.currentFloor = elevio_floorSensor();
-            elevio_floorIndicator(elevio_floorSensor());
+        if(floorState != -1){
+            heis.currentFloor = floorState;
+            elevio_floorIndicator(floorState);
             direction = 0;
             elevio_motorDirection(direction);
+            forrigeEtasje = floorState;
         
         
             // setter directrion og heis.direction
@@ -80,6 +86,15 @@ int main(){
         }
         elevio_motorDirection(direction);
 
+        if(floorState != -1 && !lysFlagg){
+            elevio_floorIndicator(floorState);
+            lysFlagg = 1;
+        }
+        else if(!lysFlagg){
+            elevio_floorIndicator(forrigeEtasje);
+        } else if(floorState == -1 && lysFlagg){
+            lysFlagg = 0;
+        }
 
         if(elevio_obstruction()){
             elevio_stopLamp(1);
@@ -92,15 +107,16 @@ int main(){
             break;
         }
         
+        // nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
         nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
 
         // printer etasjeliste for debugging
-/*        printf("elevio_floorSensor(): %d\n", elevio_floorSensor());
+        printf("floorState: %d\n", floorState);
         for(int i =0; i < 4; i++){
             printf("opp: %d  ned: %d\n", etasjeliste_opp[i], etasjeliste_ned[i]);
         }
         printf("\n");
-*/
+
         
     }
 
